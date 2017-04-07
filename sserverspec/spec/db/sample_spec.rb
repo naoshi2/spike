29ad5d2526747f721760c26f4b1fe7d5a4b42937
naoshi2 <naoc007t@yahoo.co.jp>
2017-04-07 11:38:08 -0400

    Add failure test case

--- sserverspec/spec/db/sample_spec.rb
+++ sserverspec/spec/db/sample_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
-print @property[:server_id]
+print @property[:roles]
 
 describe command('cat /etc/redhat-release') do
   its(:stdout) { is_expected.to eql("CentOS Linux release 7.3.1611 (Core) \n")}
 
-  its(:stdout) { is_expected.to match(/CentOS Linux release 7.*/)}
+  # Success
+  its(:stdout) { is_expected.to match(/CentOS Linux release [6|7].*/)}
+
+  # Fail
+  #its(:stdout) { is_expected.to match(/CentOS Linux release [5|6].*/)}
 end
 
