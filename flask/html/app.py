89fe754bf87ae57e054beb5eec021d0a2bb0a5b2
naoshi2 <naoc007t@yahoo.co.jp>
2017-05-10 23:02:48 +0900

    Add jquery sample' .

--- flask/html/app.py
+++ flask/html/app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template('index.html', message="Hello")
+	return render_template('index.html', message="Home")
+
+@app.route("/hoge")
+def hoge():
+	lst = [1, 2, 3]
+	return render_template('hoge.html', message="Hogeee", lst = lst)
 
 if __name__ == "__main__":
 	app.run()
