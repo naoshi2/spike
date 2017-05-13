418f0ff58fce2b2c01ee1d7a8621b1938affb814
naoshi2 <naoc007t@yahoo.co.jp>
2017-05-14 01:31:04 +0900

    Implement websock

--- flask/html/app.py
+++ flask/html/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_sockets import Sockets
 
 app = Flask(__name__)
+sockets = Sockets(app)
 
 @app.route("/")
 def index():
@@ -11,10 +13,18 @@ def getStr():
 
 @app.route("/hoge")
 def hoge():
-	lst = [1, 2, 3]
 	message = getStr()
-	return render_template('hoge.html', message=message, lst = lst)
+	return render_template('hoge.html', message=message)
 
+@sockets.route('/echo')
+def echo_socket(ws):
+    while True:
+        message = ws.receive()
+        ws.send(message)
 
 if __name__ == "__main__":
-	app.run()
+	#app.run()
+	from gevent import pywsgi
+	from geventwebsocket.handler import WebSocketHandler
+	server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+	server.serve_forever()
