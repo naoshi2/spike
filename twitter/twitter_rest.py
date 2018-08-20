8fe4c6064b7fd777de7d114ce6004ef65b1abc00
naoshi2 <naoc007t@yahoo.co.jp>
2018-08-20 23:20:35 +0900

    Support python3

--- twitter/twitter_rest.py
+++ twitter/twitter_rest.py
@@ -2,6 +2,7 @@
 
 import sys
 import os.path
+import json
 from twitter import *
 import configparser
 from requests_oauthlib import OAuth1Session
@@ -20,13 +21,15 @@ access_secret = parser.get('key', 'access_secret')
 consumer_key = parser.get('key', 'consumer_key')
 consumer_secret = parser.get('key', 'consumer_secret')
 
-auth = OAuth(consumer_key, consumer_secret,
-             access_key, access_secret)
+twitter = OAuth1Session(access_key, access_secret,
+                        consumer_key, consumer_secret)
 
-t = TwitterStream(auth=auth)
-tweets = t.statuses.filter(track=u"Seattle")
+url = 'https://api.twitter.com/1.1/search/tweets.json'
+params = {u'q': u'seattle', u'count': 10}
+tweets = twitter.get(url, params=params)
 
-print(tweets)
-
-for tweet in tweets:
-    print(tweet["text"].encode('utf_8'))
+search_timeline = json.loads(tweets.text)
+for tweet in search_timeline['statuses']:
+    print(tweet['user']['name'] + '::' + tweet['text'])
+    print(tweet['created_at'])
+    print('----------------------------------------------------')
